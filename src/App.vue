fd7c54a5a632934b13924da7090f348c21487214
ahmozn <ozenahmetozhan@gmail.com>
2026-03-15 14:02:42 +0300

    gotta start somewhere

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import BaseCard from './components/ui/BaseCard.vue';
+</script>
+
+<template>
+  <BaseCard>
+    <p>basecard test</p>
+  </BaseCard>
+</template>
+
+
---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="rounded-xl bg-[#212426] shadow-md w-fit min-w-[250px] h-auto flex flex-col gap-[15px] p-[1vw] pb-[1vw]">
+    <slot />
+  </div>
+</template>
