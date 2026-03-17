75b6d29545c6c7806b8a9aade6353c46deecd3aa
ahmozn <ozenahmetozhan@gmail.com>
2026-03-17 18:11:59 +0300

    simplified

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Content from './components/layout/Content.vue';
 import SiteMap from './components/layout/SiteMap.vue';
 import Summary from './components/sections/Summary/Summary.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 </script>
 
 <template>
-  <SiteMap></SiteMap>
-  <Summary></Summary>
-  <BaseCard>
-    <p>basecard test</p>
-  </BaseCard>
-
+  <Content></Content>
 </template>
 
 
