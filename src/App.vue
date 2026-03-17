6d36206318b800ff2074eb1c85d1ce84ea595896
ahmozn <ozenahmetozhan@gmail.com>
2026-03-17 18:38:57 +0300

    removed unneccessary imports

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import Content from './components/layout/Content.vue';
-import SiteMap from './components/layout/SiteMap.vue';
-import Summary from './components/sections/Summary/Summary.vue';
-import BaseCard from './components/ui/BaseCard.vue';
 </script>
 
 <template>
