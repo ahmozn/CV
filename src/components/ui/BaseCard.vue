03878f0ede28d5372c635de4d241868626ccd1a8
ahmozn <ozenahmetozhan@gmail.com>
2026-04-24 18:04:07 +0300

    fixed problem: content was exceeding borders on devices that has small widths

--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-card border border-border-base rounded-xl 
-              shadow-md w-full min-w-[250px] h-auto flex flex-col 
+              shadow-md w-full h-auto flex flex-col 
               p-5 transition-colors duration-300">
     <slot />
   </div>
